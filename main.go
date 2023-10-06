0594467ab612500996cc512de8e3f0dfe8fcee3d
VII12345 <2868914481@qq.com>
2023-10-07 00:35:33 +0800

    1

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 
 
 type TODO struct{
+	Number  string `json:"number"`
 	Content string `json:"content"`
 	Done    string `json:"done"`
 	Finish_time    string `json:"finish_time"`
@@ -22,10 +23,10 @@ func readTODO(){
 	file,_:=os.OpenFile("TODO.txt",os.O_RDWR,0666)
 	read,_:=io.ReadAll(file)
 	readline:=strings.Split(string(read),"\n")
-	lenth:=len(readline)/3
+	lenth:=len(readline)/4
 	var todo TODO
 	for i:=0;i<lenth;i++{
-		todo.Content,todo.Done,todo.Finish_time=readline[i*3],readline[i*3+1],readline[i*3+2]
+		todo.Number,todo.Content,todo.Done,todo.Finish_time=readline[i*4],readline[i*4+1],readline[i*4+2],readline[i*4+3]
 		todos=append(todos,todo)
 	}
 	file.Close()
@@ -34,6 +35,8 @@ func readTODO(){
 
 func add(text TODO){
 	file,_:=os.OpenFile("TODO.txt",os.O_APPEND,0666)
+	file.WriteString(text.Number)
+	file.WriteString("\n")
 	file.WriteString(text.Content)
 	file.WriteString("\n")
 	file.WriteString(text.Done)
@@ -47,7 +50,7 @@ func del(ind int){
 	file,_:=os.OpenFile("TODO.txt",os.O_RDWR,0666)
 	read,_:=io.ReadAll(file)
 	readline:=strings.Split(string(read),"\n")
-	readline[ind*3],readline[ind*3+1],readline[ind*3+2]="has been deleted","has been deleted","has been deleted"
+	readline[ind*4+1],readline[ind*4+2],readline[ind*4+3]=`已被删除`,`已被删除`,`已被删除`
 	file.Truncate(0)
 	file.Seek(0,0)
 	for _,i:=range readline{
@@ -61,7 +64,7 @@ func change(text TODO,ind int){
 	file,_:=os.OpenFile("TODO.txt",os.O_RDWR,0666)
 	read,_:=io.ReadAll(file)
 	readline:=strings.Split(string(read),"\n")
-	readline[ind*3],readline[ind*3+1],readline[ind*3+2]=text.Content,text.Done,text.Finish_time
+	readline[ind*4+1],readline[ind*4+2],readline[ind*4+3]=text.Content,text.Done,text.Finish_time
 	file.Truncate(0)
 	file.Seek(0,0)
 	for _,i:=range readline{
@@ -82,46 +85,73 @@ func main() {
 	r.POST("/todo",func(c *gin.Context){
 		var todo TODO
 		c.BindJSON(&todo)
+		todo.Number=strconv.Itoa(len(todos)+1)
 		todos = append(todos, todo)
 		fmt.Println(todos) 
 		add(todo)
-		c.JSON(200,gin.H{"status":"ok"})
+		c.JSON(200,gin.H{"status":"成功增加TODO"})
 	})
 
 	//删除 TODO
 	r.DELETE("/todo/:index",func (c *gin.Context)  {
 		index,_:=strconv.Atoi(c.Param("index"))
-		todos[index].Content=`has been deleted`
-		todos[index].Done=`has been deleted`
-		todos[index].Finish_time=`has been deleted`
-		del(index)
-		c.JSON(200,gin.H{"status":"ok"})
+		todos[index-1].Content=`已被删除`
+		todos[index-1].Done=`已被删除`
+		todos[index-1].Finish_time=`已被删除`
+		del(index-1)
+		c.JSON(200,gin.H{"status":"成功删除TODO"})
 	})
 
 	//更新 TODO
-	r.POST("/todo/:index",func(c *gin.Context){
+	r.PUT("/todo/:index",func(c *gin.Context){
 		index,_:=strconv.Atoi(c.Param("index"))
 		var todo TODO
 		c.BindJSON(&todo)
 		todos[index]=todo
 		change(todo,index)
-		c.JSON(200,gin.H{"status":"ok"})
+		c.JSON(200,gin.H{"status":"成功更新TODO"})
 	})
 
 	//列出 TODO
 	r.GET("/todo",func(c *gin.Context) {
-		c.JSON(200,todos)
+		page:=c.Query("page");	
+		intpage,_:=strconv.Atoi(page)
+		if (intpage-1)*20>len(todos){
+			c.JSON(200,gin.H{"status":"页码过大，您没有这么多TODO"})
+		}else if len(todos)==0 {
+			c.JSON(200,gin.H{"status":"您还未添加TODO"})
+		}else {
+			c.JSON(200,gin.H{"status":"以下是该页的TODO内容："})
+			if intpage*20>len(todos){
+				c.JSON(200,todos[20*(intpage-1):])
+			}else{
+				c.JSON(200,todos[20*(intpage-1):20*intpage])
+			}
+			
+		}
+		
 	})
 
 	//查询 TODO
 	r.GET("/todo/:index",func (c *gin.Context)  {
 		index,_:=strconv.Atoi(c.Param("index"))
 		if index>len(todos) {
-			c.JSON(200,gin.H{"status":"索引过大,请重新查询"})
+			c.JSON(200,gin.H{"status":"序号过大,请重新查询"})
 		}else {
-			c.JSON(200,todos[index])
+			c.JSON(200,gin.H{"status":"以下是您所查询的的TODO："})
+			c.JSON(200,todos[index-1])
 		}
 		
+	})
+
+	//清空 TODO
+	r.DELETE("/todo/delete",func(c *gin.Context){
+		file,_:=os.OpenFile("TODO.txt",os.O_RDWR,0666)
+		file.Truncate(0)
+		file.Seek(0,0)
+		file.Close()
+		c.JSON(200,gin.H{"status":"已成功清空TODO"})
+		todos=todos[:0]
 	})
 	
 	r.Run(":8080")
